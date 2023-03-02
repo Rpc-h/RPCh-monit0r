e0f3d01981057ca1d2a19e01b5f5b3737280c789
Martins Eglitis <sitilge@gmail.com>
2023-03-02 23:22:01 +0200

    Update paths and restore latencies.csv

--- RPCh-mon.py
+++ RPCh-mon.py
@@ -3,53 +3,75 @@ from web3 import Web3
 import time
 import matplotlib.dates as mdates
 import pandas as pd
+import matplotlib.ticker
+import datetime
 
-endpoint = "http://localhost:8080/?exit-provider=https://primary.gnosis-chain.rpc.hoprtech.net"
+def newFig():
+    plt.title("RPCh request latency over time")
+    plt.xlabel("Time")
+    plt.ylabel("Latency [s]")
+    plt.yscale("log")
+    plt.yticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20])
+    ax = plt.gca()
+    fig = plt.gcf()
+    fig.set_size_inches(7, 7)
+    xfmt = mdates.DateFormatter('%d-%m-%y %H:%M:%S')
+    fig.autofmt_xdate(rotation=45)
+    ax.xaxis.set_major_formatter(xfmt)
+    ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    ax.set_ylim(0.4, 30)
+
+exitProvider = "https://primary.gnosis-chain.rpc.hoprtech.net"
+endpoint = "http://localhost:8080/?exit-provider=" + exitProvider
 timeout = 5
 web3 = Web3(Web3.HTTPProvider(endpoint, request_kwargs={'timeout': timeout}))
 results = []
 
-plt.title("RPCh request latency over time")
-plt.xlabel("Time")
-plt.ylabel("Latency")
-plt.yscale("log")
-ax = plt.gca()
-fig = plt.gcf()
-fig.set_size_inches(7, 7)
-xfmt = mdates.DateFormatter('%d-%m-%y %H:%M:%S')
-fig.autofmt_xdate(rotation=45)
-ax.xaxis.set_major_formatter(xfmt)
+dfAll = pd.read_csv("/mnt/latencies.csv", compression="zip")
+dfAll["time"] = dfAll["time"].astype("datetime64[s]")
+dfAll = dfAll.set_index("time")
 
 while True:
-    start = time.time()
+    start = datetime.datetime.now()
     try:
         blockNo = web3.eth.get_block_number()
     except:
         blockNo = 0
-    end = time.time()
+    end = datetime.datetime.now()
     latency = end - start
-    print(f"block number: {blockNo}, took {latency:.2f}s")
-    result = {
-            "time": start,
-            "latency": latency,
-            "blockNumber": blockNo
-            }
-    results.append(result)
-    latencies = [r["latency"] for r in results if r["blockNumber"] > 0]
-    times = [r["time"] for r in results if r["blockNumber"] > 0]
-    df = pd.DataFrame({
-        "times": times,
-        "latencies": latencies
-        }, columns=["times", "latencies"])
-    df["times"] = df["times"].astype("datetime64[s]")
-    df = df.set_index("times")
+    print(f"block number: {blockNo}, took {latency}")
+    dfAll.loc[start] = [latency.total_seconds(), blockNo]
 
-    numTimeouts = len([r["latency"] for r in results if r["blockNumber"] == 0])
-    numOverThreshold = len([r["latency"] for r in results if r["blockNumber"] > 0 and r["latency"] > timeout])
-    print(f"number of requests that didn't time out: {len(latencies)}")
-    print(f"number of timeouts: {numTimeouts}")
-    print(f"number of requests that didn't time out book took over {timeout}s: {numOverThreshold}")
-    plt.plot(df["latencies"], "bo")
-    plt.savefig("/mnt/rpch.png")
+    df24h = dfAll[dfAll.index > dfAll.index[-1] - datetime.timedelta(hours=24)]
+    df1h = dfAll[dfAll.index > dfAll.index[-1] - datetime.timedelta(hours=1)]
+
+    dfAllGotResponse = dfAll[dfAll["blockNumber"] > 0]
+    dfAllNoResponse = dfAll[dfAll["blockNumber"] == 0]
+
+    df24hGotResponse = df24h[df24h["blockNumber"] > 0]
+    df24hNoResponse = df24h[df24h["blockNumber"] == 0]
+
+    df1hGotResponse = df1h[df1h["blockNumber"] > 0]
+    df1hNoResponse = df1h[df1h["blockNumber"] == 0]
+
+    plt.figure(0)
+    newFig()
+    plt.plot(dfAllGotResponse["latency"], "bx")
+    plt.plot(dfAllNoResponse["latency"], "rx")
+    plt.savefig("/mnt/latencies-all.png")
+
+    plt.figure(1)
+    newFig()
+    plt.plot(df24hGotResponse["latency"], "bx")
+    plt.plot(df24hNoResponse["latency"], "rx")
+    plt.savefig("/mnt/latencies-24h.png")
+
+    plt.figure(2)
+    newFig()
+    plt.plot(df1hGotResponse["latency"], "bx")
+    plt.plot(df1hNoResponse["latency"], "rx")
+    plt.savefig("/mnt/latencies-1h.png")
+
+    dfAll.to_csv("/mnt/latencies.csv", compression="zip")
 
     time.sleep(2)
